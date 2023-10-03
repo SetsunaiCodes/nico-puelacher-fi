8af8dd3e109498541dab4cbd51effe201d3e15c7
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2023-10-03 14:03:14 +0200

    Set Up Article Array

---
+++ blog-fi/src/aricles.js
@@ -0,0 +1,16 @@
+export const article = [
+
+    {
+        image: "/MockImage.jpg",
+        titel: "Das ist der erste Artikel",
+        beschreibung: "Das ist die erste Beschreibung",
+        content: 'Das ist der erste Content'
+    },
+    {
+        image: "/MockImage.jpg",
+        titel: "Das ist der zweite Artikel",
+        beschreibung: "Das ist die zweite Beschreibung",
+        content: 'Das ist der zweite Content'
+    }
+
+]
