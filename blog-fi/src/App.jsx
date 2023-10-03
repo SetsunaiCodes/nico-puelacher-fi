d82c04654578c6537caee0f9666b0d730d4a70c9
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2023-10-03 14:03:06 +0200

    added compoents

--- blog-fi/src/App.jsx
+++ blog-fi/src/App.jsx
@@ -1,10 +1,23 @@
+import Sidebar from './Sidebar'
+import ArtikelList from './ArtikelList'
 import './App.css'
 
 function App() {
 
   return (
-    <div>
-      
+    <div className='main-section flex'>
+      <main>
+
+      <ArtikelList/>
+
+      </main>
+
+
+      <aside>
+
+      <Sidebar/>
+
+      </aside>
     </div>
   )
 }
