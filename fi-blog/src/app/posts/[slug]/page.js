b7388b49d81127dbc13cfa74d310087ee2aadd89
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2023-10-21 10:39:53 +0200

    changed aside BarComponent

--- fi-blog/src/app/posts/[slug]/page.js
+++ fi-blog/src/app/posts/[slug]/page.js
@@ -20,30 +20,70 @@ function AsideaBar() {
   
   return(
     <div>
-          <div className='personal-card flex-a-j-center gap'>
-            <img src='/JAKEPB.jpg'/>
-              <div>
-                <p>Nico Puelacher</p>
-                <p>Programmierer</p>
-              </div>
+
+    
+    <div className='personal-card flex-a-j-center gap'>
+    <img src='/JAKEPB.jpg'/>
+      <div>
+        <p>Nico Puelacher</p>
+        <p>Programmierer</p>
+      </div>
+  </div>
+  <div className='section'>
+    <h2>Quick Links</h2>
+
+    <h3>Gedankengänge</h3>
+    <ul className='quick-links'>
+      {posts
+      .filter(post => post.id === 'Gedankengänge')
+      .map((post, idx) => (
+        <li key={idx}>
+          <div>
+            <p className='no-margin topic-quick'>{post.topic}</p>
+            <Link className='no-margin title-quick' href={post.url}>
+              {post.title}
+            </Link>
           </div>
-          <div className='section'>
-            <h3>Quick Links</h3>
-            <ul className='quick-links'>
-              {posts.map((post, idx) => (
-                <li key={idx}>
-                  <div>
-                    <p className='no-margin topic-quick'>{post.topic}</p>
-                    <Link className='no-margin title-quick' href={post.url}>
-                      {post.title}
-                    </Link>
-                  </div>
-                  <div className='divider-hor'></div>
-                </li>
-              ))}
-            </ul>
+          <div className='divider-hor'></div>
+        </li>
+      ))}
+    </ul>
+
+    <h3>Hausaufgaben</h3>
+    <ul className='quick-links'>
+      {posts
+      .filter(post => post.id === 'Hausaufgabe')
+      .map((post, idx) => (
+        <li key={idx}>
+          <div>
+            <p className='no-margin topic-quick'>{post.topic}</p>
+            <Link className='no-margin title-quick' href={post.url}>
+              {post.title}
+            </Link>
           </div>
-    </div>
+          <div className='divider-hor'></div>
+        </li>
+      ))}
+    </ul>
+
+    <h3>DevLogs</h3>
+    <ul className='quick-links'>
+      {posts
+      .filter(post => post.id === 'DevLog')
+      .map((post, idx) => (
+        <li key={idx}>
+          <div>
+            <p className='no-margin topic-quick'>{post.topic}</p>
+            <Link className='no-margin title-quick' href={post.url}>
+              {post.title}
+            </Link>
+          </div>
+          <div className='divider-hor'></div>
+        </li>
+      ))}
+    </ul>
+  </div>
+  </div>
   )
 }
 
